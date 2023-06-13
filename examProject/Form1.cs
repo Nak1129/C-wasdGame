96f59b7dd735e80c7f468a16cd86fd88251485b1
Nak <leoyang891129@gmail.com>
2023-06-13 21:13:41 +0800

    期末作業

---
+++ examProject/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using Microsoft.VisualBasic.Devices;
+
+namespace examProject
+{
+    public partial class Form1 : Form
+    {
+        
+        int time;
+        int correct;
+        int uncorrect;   
+        
+        public Form1()
+        {
+            InitializeComponent();
+            //介面  
+            timer1.Enabled = false;
+            label8.Visible = false;
+            texchage();
+            
+        }
+        
+        public void texchage()
+        {
+            Random random = new Random();
+            string[] characters = { "W", "S", "A", "D" };
+            string rd_char = characters[random.Next(0, characters.Length)];
+            label8.Text = rd_char;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Form2 lForm = new Form2();//實例化一個Form2窗口  
+
+            int[] pointArray = { int.Parse(label9.Text) , int.Parse(label10.Text) };
+            lForm.GetFrom1Point = pointArray;  //設置Form2中string1的值  
+            
+            lForm.GetFrom1Rate = float.Parse(label11.Text);
+
+            lForm.SetValue();//設置Form2中Label1的  
+            lForm.ShowDialog();
+            
+        }
+
+        //計算
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            {
+                //壓值變色
+                if (e.KeyCode == Keys.W)
+                {
+                    label1.ForeColor = Color.Red;
+                }
+                else
+                {
+                    label1.ForeColor = Color.Black;
+                }
+                if (e.KeyCode == Keys.S)
+                {
+                    label2.ForeColor = Color.Red;
+                }
+                else
+                {
+                    label2.ForeColor = Color.Black;
+                }
+                if (e.KeyCode == Keys.A)
+                {
+                    label3.ForeColor = Color.Red;
+                }
+                else
+                {
+                    label3.ForeColor = Color.Black;
+                }
+                if (e.KeyCode == Keys.D)
+                {
+                    label4.ForeColor = Color.Red;
+                }
+                else
+                {
+                    label4.ForeColor = Color.Black;
+                }
+            }
+            if (timer1.Enabled == true)
+            {
+                //如按鈕 == rd_char就馬上換字
+                if (e.KeyCode.ToString() == label8.Text)
+                {
+                    //正確次數
+                    correct++;                    
+                    texchage();
+                    label9.Text = correct.ToString();
+                }
+                else if (e.KeyCode.ToString() != label8.Text)
+                {
+                    uncorrect++;
+                    label10.Text = uncorrect.ToString();
+                }              
+               
+            }
+        }
+
+        //倒數計時
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Interval = 1000;             
+            if (time > 0)
+            {
+                time--;
+                label7.Text = time.ToString();
+
+                //先蓋住答案
+                label8.Visible = true;
+
+                float  correct   = int.Parse(label9.Text);
+                float uncorrect = int.Parse(label10.Text);
+                float rate  = (correct / (correct + uncorrect))*100 ;
+                string rate_string = rate.ToString("#.##");
+                label11.Text  = rate_string;
+            }            
+            else
+            {                               
+                timer1.Enabled = false;
+                MessageBox.Show("time up");
+                button1.Enabled = true;
+            }         
+
+        }       
+
+        //開始按鈕
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //外表
+            label1.Text = "W";
+            label2.Text = "S";
+            label3.Text = "A";
+            label4.Text = "D";
+
+            //初始化
+            correct = 0;
+            label9.Text = correct.ToString();
+            uncorrect = 0;
+            label10.Text = uncorrect.ToString();
+
+            timer1.Enabled = true;            
+            button1.Enabled = false; 
+            
+            time = 10;            
+            label7.Text = time.ToString();
+        }
+               
+    }
+}
---
+++ examProject/Form2.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using Microsoft.VisualBasic;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace examProject
+{
+    public partial class Form2 : Form
+    {
+        string str;
+        float rate;
+        int [] GetPoint = new int[2];
+
+        public Form2()
+        {
+            InitializeComponent();
+            label2.Text = Interaction.InputBox("您的名字", "名字輸入框");
+
+            label3.Text = "";
+            label4.Text = "";
+            
+        }
+
+        //抓正確和錯誤 次數
+        public int[] GetFrom1Point
+        {
+            set
+            {
+                GetPoint[0] = value[0];
+                GetPoint[1] = value[1];
+            }
+        }
+        //抓正確率
+        public float GetFrom1Rate
+        {
+            set
+            {
+                rate = value;
+            }
+        }
+
+        public void SetValue()
+        {
+            this.label1.Text = GetPoint[0].ToString();
+            this.label12.Text = GetPoint[1].ToString();
+            this.label13.Text = rate.ToString();
+        }
+
+
+        
+        public SqlConnection GetSqlCon()
+        {
+            //待修改
+            // string str = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename "+ Application.StartupPath+ @"\rank.mdf;";
+            string str = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Nak\Downloads\C-wasdGame-master\C-wasdGame-master\examProject\rank.mdf;";
+            SqlConnection sqlCon = new SqlConnection(str);
+            sqlCon.Open();
+            return sqlCon;            
+        }      
+       
+        
+        
+
+        //新增
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string name = label2.Text;
+
+                int correct = int.Parse(label1.Text);
+                int uncorrect = int.Parse(label12.Text);                
+                //float test_float = (rate);                
+                
+                SqlConnection sqlCon = GetSqlCon();              
+                string sql_insert = "insert into player (Name,Correct,Uncorrect,rate) values(N'" + name + "','" + correct + "','" + uncorrect + "','" + rate + "')";
+                SqlCommand cmd = new SqlCommand(sql_insert, sqlCon);
+                //執行指令 並讓系統讀取                
+                if (cmd.ExecuteNonQuery() > 0)
+                {
+                    MessageBox.Show("新增成功");
+                    //限制新增一次
+                    button2.Enabled = false;
+                }
+                else
+                {
+                    MessageBox.Show("新增失敗");
+                }
+              
+                string sql_searchOwn = "select * from player where Name= N'" + name + "' and Correct='" + correct + "' and Uncorrect='" + uncorrect + "'and rate='"+ rate + "'" ;
+                SqlCommand cmd_searchOwn = new SqlCommand(sql_searchOwn, sqlCon);
+                SqlDataReader sql_dr = cmd_searchOwn.ExecuteReader();
+                while (sql_dr.Read())
+                {
+                    label4.Text = string.Format("ID:{0} 名字:{1}正確:{2} 錯誤:{3}正確率{4} ", sql_dr.GetInt32(0), sql_dr.GetString(1), sql_dr.GetInt32(2), sql_dr.GetInt32(3), sql_dr.GetDouble(4));
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+
+        //查詢自己
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int tb_id = int.Parse(textBox2.Text);
+                string tb_name = textBox3.Text;
+
+                SqlConnection sqlCon = GetSqlCon();
+                string sql_searchOwn = "select * from player where Id='"+ tb_id +"' and Name = N'"+ tb_name +"'";
+                SqlCommand cmd_searchOwn = new SqlCommand(sql_searchOwn, sqlCon);
+                SqlDataReader sql_dr = cmd_searchOwn.ExecuteReader();
+                if (!sql_dr.HasRows)
+                {
+                    MessageBox.Show("查無此人", "未達到");
+                    label7.Text = "";
+                }
+                while (sql_dr.Read())
+                {
+                    //string.Format("自己想加標頭", 抓取table 內容)
+                    label7.Text = string.Format("ID:{0}名字:{1}正確:{2}錯誤:{3}正確率{4}",sql_dr.GetInt32(0),sql_dr.GetString(1),sql_dr.GetInt32(2),sql_dr.GetInt32(3),sql_dr.GetDouble(4));
+                }
+
+                //清空
+                textBox2.Text = "";
+                textBox3.Text = "";
+            }
+            catch(Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+        //修改
+        private void button6_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int db_id = int.Parse(textBox1.Text);                
+                string changeNameAfter = textBox5.Text;
+
+                SqlConnection sqlCon = GetSqlCon();
+                string updateOwn = "update player set Name= N'" + changeNameAfter + "'where Id='" + db_id + "'";
+                SqlCommand cmd = new SqlCommand(updateOwn, sqlCon);
+                if (cmd.ExecuteNonQuery() > 0)
+                {
+                    MessageBox.Show("修改成功");
+                    textBox1.Text = "";
+                    textBox5.Text = "";
+                    label2.Text = changeNameAfter ;
+                }
+                else
+                {
+                    MessageBox.Show("修改失敗");
+                    label9.Text = "";
+                    textBox1.Text = "";
+                    textBox5.Text = "";
+                }
+                string sql_search = "select * from player where Id='" + db_id + "'";
+                SqlCommand cmd_d = new SqlCommand(sql_search, sqlCon);
+                //執行指令 並讓系統讀取
+                SqlDataReader sql_dr = cmd_d.ExecuteReader();
+                while (sql_dr.Read())
+                {
+                    //string.Format("自己想加標頭", 抓取table 內容)
+                    label4.Text = string.Format("ID:{0} 名字:{1}",sql_dr.GetInt32(0),sql_dr.GetString(1));
+                    label9.Text = "更改後的名字  "+label4.Text;
+                }
+            }
+            catch(Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+            
+        }
+        //刪除指定自己
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int db_id = int.Parse(textBox4.Text);
+                SqlConnection sqlCon = GetSqlCon();
+
+                string deleteOwn = "DELETE FROM player WHERE Id='" + db_id + "'";
+                SqlCommand cmd = new SqlCommand(deleteOwn, sqlCon);
+                if (cmd.ExecuteNonQuery() > 0)
+                {
+                    MessageBox.Show("刪除成功");
+                    textBox4.Text = "";
+                    label4.Text = "資料被刪除了";
+                    label7.Text = "";
+                    label2.Text = "?_?";
+                }
+                else
+                {
+                    MessageBox.Show("刪除失敗");
+                    textBox4.Text = "";
+                }
+            }
+            catch(Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+        //正確rank
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                //前置作業
+                label3.Text = "     Rank CorrectPoint Top 5 \n";
+                SqlConnection sqlCon = GetSqlCon();
+
+                string sql_search = "select top 5* from player  ORDER BY Correct DESC";
+                SqlCommand cmd = new SqlCommand(sql_search, sqlCon);
+                //執行指令 並讓系統讀取
+                SqlDataReader sql_dr = cmd.ExecuteReader();
+                while (sql_dr.Read())
+                {
+                    str = string.Format("ID:{0}名字:{1}正確:{2}", sql_dr.GetInt32(0), sql_dr.GetString(1), sql_dr.GetInt32(2));
+                    label3.Text += str + "\n";
+                }
+                sqlCon.Close();
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+
+        }
+        //錯誤rank
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //前置作業
+                label3.Text = "     Rank UncorrectPoint Top 5 \n";
+                SqlConnection sqlCon = GetSqlCon();  
+                
+                string sql_search = "select top 5* from player  ORDER BY UNcorrect DESC";
+                SqlCommand cmd = new SqlCommand(sql_search, sqlCon);
+                //執行指令 並讓系統讀取
+                SqlDataReader sql_dr = cmd.ExecuteReader();
+                while (sql_dr.Read())
+                {
+                    str = string.Format("ID:{0}名字:{1}正確:{2}錯誤:{3}",sql_dr.GetInt32(0),sql_dr.GetString(1),sql_dr.GetInt32(2), sql_dr.GetInt32(3));
+                    label3.Text += "ID:"+ sql_dr.GetInt32(0)+ "名字:" + sql_dr.GetString(1)+ "錯誤:" + sql_dr.GetInt32(3) + "\n";
+                }
+                  sqlCon.Close();
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+
+        //正確率排行版
+        private void button7_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //前置作業
+                label3.Text = "     Rank Rate Top 5 \n";
+                SqlConnection sqlCon = GetSqlCon();               
+                string sql_search = "select top 5* from player  ORDER BY rate DESC";
+                SqlCommand cmd = new SqlCommand(sql_search, sqlCon);
+                //執行指令 並讓系統讀取
+                SqlDataReader sql_dr = cmd.ExecuteReader();
+                while (sql_dr.Read())
+                {
+                    str = string.Format("ID:{0}名字:{1}正確:{2}錯誤:{3}正確率{4}", sql_dr.GetInt32(0), sql_dr.GetString(1), sql_dr.GetInt32(2), sql_dr.GetInt32(3), sql_dr.GetDouble(4));
+                    label3.Text += "ID:" + sql_dr.GetInt32(0) + "名字:" + sql_dr.GetString(1) + "正確率:" + sql_dr.GetDouble(4) + "\n";
+                }
+                sqlCon.Close();
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+    }
+}
